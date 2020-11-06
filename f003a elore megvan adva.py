d331bcf3d246e4477ce375b61cf84b33fc90f94f
Csongor00 <71691734+Csongor00@users.noreply.github.com>
2020-11-06 08:27:21 +0100

    Add files via upload

---
+++ f003a elore megvan adva.py
@@ -0,0 +1,4 @@
+vezetek = input("Mi a vezetek neved"'\n')
+print('Az en vezetek nevem Csongor',vezetek,'\n')
+kereszt = input("Mi a kereszt neved"'\n')
+print("Az en kereszt nevem Szilagyi" ,kereszt,)
