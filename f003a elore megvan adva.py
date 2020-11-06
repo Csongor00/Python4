680fd2b76cfa94e91aa8513a554f6bb6d0510675
Csongor00 <71691734+Csongor00@users.noreply.github.com>
2020-11-06 08:34:32 +0100

    Delete f003a elore megvan adva.py

--- f003a elore megvan adva.py
+++
@@ -1,4 +0,0 @@
-vezetek = input("Mi a vezetek neved"'\n')
-print('Az en vezetek nevem Csongor',vezetek,'\n')
-kereszt = input("Mi a kereszt neved"'\n')
-print("Az en kereszt nevem Szilagyi" ,kereszt,)
